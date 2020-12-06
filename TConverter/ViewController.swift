175520fdab3c2752a7d83a85790b2302edc3b755
IsaikinSergei <akinfest007@gmail.com>
2020-12-06 21:35:32 +0400

    Initial Commit

---
+++ TConverter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TConverter
+//
+//  Created by Sergei Isaikin on 06.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var celsiusLabel: UILabel!
+    @IBOutlet weak var fahrenheitLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    
+    
+    
+    
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
+    }
+    
+
+}
+
